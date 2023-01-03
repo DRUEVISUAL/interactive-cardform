1116249b5874f518b6847b742832cce8fabbb559
DRUEVISUAL <druevisual@gmail.com>
2023-01-03 21:08:11 +0100

    Fiexed blinking section overlay issue

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ let cardName = document.querySelector('#header_card_front_name')
 let expDateMonth = document.querySelector('#header_card_front_date_MM')
 let expDateYear = document.querySelector('#header_card_front_date_YY')
 
+let arr = []
+
+let defaultName = 'Name Surename'
+let defaultNumber = ['0000000000000000']
+let defaultMonth = '00'
+let defaultYear = '00'
+let defaultCVC = '000'
+
 const inputName = document.querySelector('#full_name')
 const inputExpDateMonth = document.querySelector('#exp_date_MM')
 const inputExpDateYear = document.querySelector('#exp_date_YY')
@@ -20,9 +28,6 @@ const thankyouDisplay = document.querySelector('#thanks_panel')
 const headerDisplay = document.querySelector('#header')
 const asideDisplay = document.querySelector('#aside')
 
-let arr = document.getElementsByTagName("input");
-let newArr = []
-
 window.addEventListener('load', () => {
     thankyouDisplay.style.display = 'none';
     asideDisplay.style.display = 'none';
@@ -30,26 +35,52 @@ window.addEventListener('load', () => {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    inputToCard()
-    cardNumberSeparator()
     formDisplay.hidden = true;
     thankyouDisplay.style.display = 'flex';
 })
 
 inputName.addEventListener('keyup',()=>{
     mirrorIfValid(validityChecker(inputName), cardName, inputName)
+
+    checkIfBlank(inputName, cardName, defaultName)
+})
+
+inputNumber.addEventListener('keyup',()=>{
+    if (validityChecker(inputNumber)){
+        arr = [inputNumber.value]
+    } validityChecker(inputNumber)
+
+    if (validityChecker(inputNumber)){
+        cardNumberSeparator()
+    }
+
+    if (inputNumber.value === ""){
+        inputNumber.parentElement.classList.add('blank')
+        numA.innerHTML = defaultNumber[0].slice(0, 4)
+        numB.innerHTML = defaultNumber[0].slice(4, 8)
+        numC.innerHTML = defaultNumber[0].slice(8, 12)
+        numD.innerHTML = defaultNumber[0].slice(12, 16)
+    } else {
+        inputNumber.parentElement.classList.remove('blank')
+    }
 })
 
 inputExpDateMonth.addEventListener('keyup',()=>{
     mirrorIfValid(validityChecker(inputExpDateMonth), expDateMonth, inputExpDateMonth)
+
+    checkIfBlank(inputExpDateMonth, expDateMonth, defaultMonth)
 })
 
 inputExpDateYear.addEventListener('keyup',()=>{
     mirrorIfValid(validityChecker(inputExpDateYear), expDateYear, inputExpDateYear)
+
+    checkIfBlank(inputExpDateYear, expDateYear, defaultYear)
 })
 
 inputCVC.addEventListener('keyup',()=>{
     mirrorIfValid(validityChecker(inputCVC), cardCVC, inputCVC)
+
+    checkIfBlank(inputCVC, cardCVC, defaultCVC)
 })
 
 continueButton.addEventListener('click', () => {
@@ -57,6 +88,15 @@ continueButton.addEventListener('click', () => {
     headerDisplay.style.display = 'none';
     asideDisplay.style.display = 'flex';
 })
+
+function checkIfBlank(inp, field, defaultField){
+    if (inp.value === ""){
+        inp.parentElement.classList.add('blank')
+        field.innerHTML = defaultField
+    } else {
+        inp.parentElement.classList.remove('blank')
+    }
+}
 
 function validityChecker(inp) {
     if (inp.validity.patternMismatch){
@@ -86,10 +126,8 @@ function inputToCard() {
 }
 
 function cardNumberSeparator() {
-    newArr = [arr[1].value]
-    
-    numA.innerHTML = newArr[0].slice(0, 4)
-    numB.innerHTML = newArr[0].slice(4, 8)
-    numC.innerHTML = newArr[0].slice(8, 12)
-    numD.innerHTML = newArr[0].slice(12, 16)
+    numA.innerHTML = arr[0].slice(0, 4)
+    numB.innerHTML = arr[0].slice(4, 8)
+    numC.innerHTML = arr[0].slice(8, 12)
+    numD.innerHTML = arr[0].slice(12, 16)
 }
